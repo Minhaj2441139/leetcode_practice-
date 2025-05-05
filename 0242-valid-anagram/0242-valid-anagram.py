c9e39fa91a603f6b345ab33cb4c13a4462990cbc
noob_73 <69976635+Minhaj2441139@users.noreply.github.com>
2025-05-06 01:00:23 +0600

    Time: 11 ms (72.48%), Space: 17.9 MB (63.89%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,15 @@
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        sorted_s = sorted(s)
-        sorted_t = sorted(t)
-        return sorted_s == sorted_t
+    def isAnagram(self, s: str, t: str) -> bool:  
+        if len(s)!=len(t):
+            return False
+        
+        count = [0]*26
+        for i in range(len(s)):
+            count[ord(s[i])-ord('a')]+=1
+            count[ord(t[i])-ord('a')]-=1
+
+        for val in count:
+            if val!=0:
+                return False
+            
+        return True

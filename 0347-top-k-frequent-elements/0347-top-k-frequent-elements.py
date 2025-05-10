1fded409594843ce26c57c3fb58b031af5177127
noob_73 <69976635+Minhaj2441139@users.noreply.github.com>
2025-05-10 20:16:40 +0600

    Time: 4 ms (69.32%), Space: 21.4 MB (40.18%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,11 +1,8 @@
 class Solution:
-	def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-		frequency = {}
-		for num in nums:
-			if num not in frequency:
-				frequency[num] = 1
-			else:
-				frequency[num] = frequency[num] + 1
-		frequency = dict(sorted(frequency.items(), key=lambda x: x[1], reverse=True))
-		result = list(frequency.keys())[:k]
-		return result
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Count frequencies
+        a = Counter(nums)
+        # Get the k most common elements as a list of (element, count) pairs
+        most_common = a.most_common(k)
+        # Extract just the elements (keys), ignoring counts
+        return [item[0] for item in most_common]

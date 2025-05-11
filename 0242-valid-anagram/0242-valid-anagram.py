28f60e8a0823a25b9850834f1b74e044d073b056
noob_73 <69976635+Minhaj2441139@users.noreply.github.com>
2025-05-11 14:18:38 +0600

    Time: 13 ms (49.38%), Space: 17.6 MB (97.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -8,8 +8,4 @@ class Solution:
             count[ord(s[i])-ord('a')]+=1
             count[ord(t[i])-ord('a')]-=1
 
-        for val in count:
-            if val!=0:
-                return False
-            
-        return True
+        return not any(count)

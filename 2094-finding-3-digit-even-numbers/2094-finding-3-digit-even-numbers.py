7b11aa3af21cb3fc625fb28f22dd1a057990cd1c
noob_73 <69976635+Minhaj2441139@users.noreply.github.com>
2025-05-13 01:35:19 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.py
@@ -1,22 +1,9 @@
 class Solution:
     def findEvenNumbers(self, digits: List[int]) -> List[int]:
-        cnt = Counter(digits)
-        evens = [d for d in cnt if d % 2 == 0]
-        res = set()
-
-        # a = hundreds place (≠0), b = tens place, c = ones place (even)
-        for a in cnt:
-            if a == 0:                   # no leading zero
-                continue
-            for b in cnt:
-                # need at least two of the same digit if a==b
-                if a == b and cnt[b] < 2:
-                    continue
-                for c in evens:
-                    # build a small counter of how many times we use each digit
-                    need = Counter((a, b, c))
-                    # check availability
-                    if all(cnt[d] >= need[d] for d in need):
-                        res.add(100*a + 10*b + c)
-
-        return sorted(res)
+        result = {
+        100*a + 10*b + c
+        for a, b, c in permutations(digits, 3)
+        if a != 0      # no leading zero
+        and c % 2 == 0 # last digit even
+    }
+    return sorted(result)

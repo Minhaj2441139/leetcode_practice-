41d4094a26d2056177e9b5f3a642be310797ad40
noob_73 <69976635+Minhaj2441139@users.noreply.github.com>
2025-05-14 01:05:30 +0600

    Time: 368 ms (89.54%), Space: 22.5 MB (24.18%) - LeetHub

--- 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.py
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.py
@@ -1,24 +1,9 @@
 class Solution(object):
     def lengthAfterTransformations(self, s, t):
         mod = 10**9 + 7
-        nums = [0]*26
-        for ch in s:
-            nums[ord(ch) - 97] += 1
-
-        for _ in range(t):
-            cur = [0]*26
-            z = nums[25]
-            if z:
-                # 'z' → 'a' and 'b'
-                cur[0] = (cur[0] + z) % mod
-                cur[1] = (cur[1] + z) % mod
-            for j in range(25):
-                v = nums[j]
-                if v:
-                    cur[j+1] = (cur[j+1] + v) % mod
-            nums = cur
-
-        res = 0
-        for v in nums:
-            res = (res + v) % mod
-        return res
+        dp = [0] * (t + 26)
+        for i in range(26): dp[i] = 1
+        for i in range(26, t + 26): dp[i] = (dp[i - 26] + dp[i - 25]) % mod
+        ans = 0
+        for ch in s: ans = (ans + dp[ord(ch) - ord('a') + t]) % mod
+        return ans
